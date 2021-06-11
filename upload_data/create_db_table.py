0af29115ea3b093388b5c0a595d0df62c42f125e
olahsymbo <olahsymbo@gmail.com>
2021-06-11 22:32:31 +0100

    add secret files

--- upload_data/create_db_table.py
+++ upload_data/create_db_table.py
@@ -1,27 +1,32 @@
+import os
 import psycopg2
-from credentials_db import *
+from dotenv import load_dotenv
 import warnings
+
 warnings.filterwarnings("ignore")
 
+load_dotenv()
 # get the postgres db connection parameters from environment variable
-name = os.environ.get('name')
-user = os.environ.get('user')
-password = os.environ.get('password')
-host = os.environ.get('host')
-port = os.environ.get('port')
+name = os.getenv("DATABASE_NAME")
+user = os.getenv("DATABASE_USERNAME")
+password = os.getenv("DATABASE_PASSWORD")
+host = os.getenv("DATABASE_HOST")
+port = os.getenv("DATABASE_PORT")
 
 # connect an instance of postgres DB
-con = psycopg2.connect(database= name, user= user, password= password, host= host, port= port)
+con = psycopg2.connect(
+    database=name, user=user, password=password, host=host, port=port
+)
 print("Database opened successfully")
 
 cur = con.cursor()
 
 # create a table "Taskdata" in db "task" in our postgres
 cur.execute('''CREATE TABLE Taskdata
-      (id VARCHAR(250) PRIMARY KEY, 
+      (id VARCHAR(250) PRIMARY KEY,
       timestamp TIMESTAMP,
       temperature VARCHAR(250) NOT NULL,
       duration VARCHAR(250) NOT NULL);''')
 print("Table created successfully")
 con.commit()
-con.close() # commit and close connection
+con.close()  # commit and close connection
--- upload_data/credentials_db.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Set environment variables for other
-os.environ['engine'] = 'django.db.backends.postgresql_psycopg2'
-os.environ['name'] = 'task'
-os.environ['user'] = 'admin1'
-os.environ['password'] = 'admin1'
-os.environ['host'] = 'localhost'
-os.environ['port'] = '5432'
