f019de9f6f939cee306233499e155e1542fc4bff
olahsymbo <olahsymbo@gmail.com>
2021-06-12 12:11:55 +0100

    add secret files

--- showdata/tests.py
+++ showdata/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Taskdata
+
+
+class DataTaskTest(TestCase):
+    def setUp(self):
+        self.entry = Taskdata.objects.create(
+            timestamp=timezone.now().strftime("%Y-%m-%d"),
+            temperature="hot",
+            duration="ten"
+        )
+
+    def test_data_entry(self):
+        new_entry = self.entry
+        self.assertTrue(isinstance(new_entry, Taskdata))
--- upload_data/stack_data.py
+++ upload_data/stack_data.py
@@ -1,53 +1,40 @@
-'''
+"""
 this application loads the task.csv data to the table Taskdata in
 postgres DB
-'''
+"""
 import os
 import sys
 import csv
 import inspect
-import pandas as pd
 
-app_path = inspect.getfile(inspect.currentframe()) # gets the current file directory
-sub_dir = os.path.realpath(os.path.dirname(app_path)) # gets the file preceding directory
-main_dir = os.path.dirname(sub_dir) # gets the main directory
 
-csv_file = os.path.join(main_dir, "task/task_data.csv") # gets the directory of the task.csv file
+app_path = inspect.getfile(inspect.currentframe())  # gets the current file directory
+sub_dir = os.path.realpath(os.path.dirname(app_path))  # gets the file preceding directory
+main_dir = os.path.dirname(sub_dir)  # gets the main directory
+
+csv_file = os.path.join(main_dir, "task/task_data.csv")  # gets the directory of the task.csv file
 
 project_home = main_dir
 
 sys.path.append(project_home)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DataTask.settings") # get django setting properties
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DataTask.settings")  # get django setting properties
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DataTask.settings'
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application() # get the django web application server.
+from django.core.wsgi import get_wsgi_application  # noqa: E402
+application = get_wsgi_application()  # get the django web application server.
 
-from showdata.models import Taskdata
-# from showdata.serializers import TaskSerializer
-# 
-# ArticleSet = Taskdata.objects.all()
-#
-# article_attr = []
-#
-# for Article in ArticleSet:
-#     cont = [Article.id, Article.timestamp, Article.temperature, Article.duration]
-#     article_attr.append(cont)
-#
-# print(pd.DataFrame(article_attr))
+from showdata.models import Taskdata  # noqa: E402
 
 with open(csv_file, 'r') as csvfile:
-    data_loader = csv.reader(csvfile) # reads the task.csv data
-    #print(data_loader)
-    next(data_loader) # skips the header names on the csv data
+    data_loader = csv.reader(csvfile)  # reads the task.csv data
+    next(data_loader)  # skips the header names on the csv data
 
     # create a data object and inputs the data of each columns in the table,
     # this will store all data into the DB table.
     for ind in data_loader:
-        #print(ind[1])
         Taskdata.objects.create(
-            id = ind[0],
-            timestamp = ind[1],
-            temperature = ind[2],
-            duration = ind[3])
+            id=ind[0],
+            timestamp=ind[1],
+            temperature=ind[2],
+            duration=ind[3])
