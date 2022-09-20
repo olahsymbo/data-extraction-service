419a90367156b4cf53e2aa52daeab2a697adee91
olahsymbo <olahsymbo@gmail.com>
2022-09-20 13:49:50 +0100

    change project name

--- data_extraction_service/urls.py
+++ data_extraction_service/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
